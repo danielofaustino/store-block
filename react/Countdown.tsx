a8e888fe28ef0914375d7c351b33a4a93e1df64e
Tecnologia02 <tecnologia02@intranet.local>
2020-08-14 08:57:06 -0300

    Componetizando o bloco contador

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -2,34 +2,31 @@ import React, { useState } from 'react'
 import { TimeSplit } from './typings/global'
 import { tick } from './utils/time'
 import { useCssHandles } from 'vtex.css-handles'
-import { FormattedMessage } from 'react-intl'
+
 
 interface CountdownProps { 
-  title: string
   targetDate: string 
 }
-const CSS_HANDLES = ["container", "countdown", "title"]
+const CSS_HANDLES = ["countdown"]
 
-
+const DEFAULT_TARGET_DATE = (new Date('2020-03-02')).toISOString()
 const Countdown: StorefrontFunctionComponent<CountdownProps> = ({  
-  title,
-  targetDate,
+  targetDate = DEFAULT_TARGET_DATE,
  }) => {
   const [timeRemaining, setTime] = useState<TimeSplit>({
     hours: '00',
     minutes: '00',
     seconds: '00'
 })
-  const titleText = title || <FormattedMessage id="countdown.title" />
+  
   const handles = useCssHandles(CSS_HANDLES)
   tick(targetDate, setTime)
   return (
-    <div className={`${handles.container} t-heading-2 fw3 w-100 c-muted-1`}>
-      <div className={`${handles.title} db tc`}>{titleText}</div>
+    
       <div className={`${handles.countdown} db tc`}>
         {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
       </div>
-    </div>
+   
   )
 }
 
@@ -38,17 +35,12 @@ Countdown.schema = {
   description: 'editor.countdown.description',
   type: 'object',
   properties: {
-    title: {
-      title: 'Sou um título',
+    targetDate: {
+      title: 'editor.countdown.targetDate.title',
+      description: 'editor.countdown.targetDate.description',
       type: 'string',
       default: null,
-         },
-    targetDate:{
-      title: 'Data final',
-      description: 'Data final utilizada no contador',
-      type: 'string',
-      default: null
-    }
+    },
   },
 }
 
